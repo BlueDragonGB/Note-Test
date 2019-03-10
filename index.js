eeb94691cbfe31a6ea35c5bd347a630a0db273f0
Stephen M <stephenjmullen93@gmail.com>
2019-03-10 14:43:53 +0000

    initial Commit

---
+++ index.js
@@ -0,0 +1,30 @@
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+var path = require('path');
+const PORT = process.env.PORT || 5000;
+
+http.createServer(function(req, res){
+  var q = url.parse(req.url, true);
+  console.log(q.pathname);
+  var fileName = '.'+ q.pathname;
+  if(fileName=='./'){fileName = './main'}
+
+  // if(fileName.includes('.html')==false){
+  //   if(fs.exists(fileName + ".html"){
+  //     fileName = fileName +'.html';
+  //   }
+  // }
+  fs.readFile(fileName, function (err, data){
+    if(err){
+      res.writeHead(404,{'Content-Type': 'text/html'});
+      return res.end('404 not found' + fileName)
+    }
+    res.writeHead(200,{'Content-Type': 'text/html'});
+    res.write(data);
+    console.log('...incoming request'+ req.url);
+    res.end();
+  })
+}).listen(PORT);
+
+console.log('server listening on '+ PORT);
